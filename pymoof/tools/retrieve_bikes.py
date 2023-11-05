3e3881cd00cb28168ca638e10aa4ced3126ede83
Stuart Pearson <stuart.pearson@philips.com>
2023-11-05 14:53:35 +0100

    Added retrieve_bikes

---
+++ pymoof/tools/retrieve_bikes.py
@@ -0,0 +1,40 @@
+import base64
+import getpass
+
+import requests
+
+
+def query(username, password):
+    API_URL = "https://my.vanmoof.com/api/v8"
+
+    # This api key is distributed by the official Vanmoof
+    # app and as far as I can tell is universally the same on everyone's phone.
+    API_KEY = "fcb38d47-f14b-30cf-843b-26283f6a5819"
+
+    headers = {
+        "Api-Key": API_KEY,
+        "Authorization": "Basic "
+        + base64.b64encode((username + ":" + password).encode()).decode("ascii"),
+    }
+
+    result = requests.post(API_URL + "/authenticate", headers=headers)
+    result = result.json()
+
+    if "error" in result:
+        raise Exception("error", result)
+
+    token = result["token"]
+
+    headers = {
+        "Api-Key": API_KEY,
+        "Authorization": "Bearer " + token,
+    }
+
+    result = requests.get(
+        API_URL + "/getCustomerData",
+        headers=headers,
+        params={"includeBikeDetails": ""},
+    )
+    result = result.json()
+
+    return result
--- pymoof/tools/retrieve_encryption_key.py
+++ pymoof/tools/retrieve_encryption_key.py
@@ -3,46 +3,15 @@ import getpass
 
 import requests
 
+from pymoof.tools import retrieve_bikes
 
 def query(username, password):
-    print("Getting key from vanmoof servers")
-    API_URL = "https://my.vanmoof.com/api/v8"
-
-    # This api key is distributed by the official Vanmoof
-    # app and as far as I can tell is universally the same on everyone's phone.
-    API_KEY = "fcb38d47-f14b-30cf-843b-26283f6a5819"
-
-    headers = {
-        "Api-Key": API_KEY,
-        "Authorization": "Basic "
-        + base64.b64encode((username + ":" + password).encode()).decode("ascii"),
-    }
-
-    result = requests.post(API_URL + "/authenticate", headers=headers)
-    result = result.json()
-
-    if "error" in result:
-        raise Exception("error", result)
-
-    token = result["token"]
-
-    headers = {
-        "Api-Key": API_KEY,
-        "Authorization": "Bearer " + token,
-    }
-
-    result = requests.get(
-        API_URL + "/getCustomerData",
-        headers=headers,
-        params={"includeBikeDetails": ""},
-    )
-    result = result.json()
-
+    result = retrieve_bikes.query(username, password)
+    print("Got bikes: %s", result["data"]["bikeDetails"])
     # Only get the first bike's encryption key
     encryption_key = result["data"]["bikeDetails"][0]["key"]["encryptionKey"]
     user_key_id = result["data"]["bikeDetails"][0]["key"]["userKeyId"]
     return encryption_key, user_key_id
-
 
 if __name__ == "__main__":
     print(query())
