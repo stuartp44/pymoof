1c4509b7f3e1adc867bf217e6f668d8804b1d6d2
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2024-04-15 17:29:03 +0000

    Refactored print

--- pymoof/tools/retrieve_encryption_key.py
+++ pymoof/tools/retrieve_encryption_key.py
@@ -7,7 +7,7 @@ from pymoof.tools import retrieve_bikes
 
 def query(username, password):
     result = retrieve_bikes.query(username, password)
-    print(f"Got bikes: {result['data']['bikeDetails']}")
+    print("Got bikes:", result)
     # Only get the first bike's encryption key
     encryption_key = result[0]["key"]["encryptionKey"]
     user_key_id = result[0]["key"]["userKeyId"]
