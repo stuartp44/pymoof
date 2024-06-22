05a8ad03abb5c9bee5da49bafe8a57e043703172
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2024-06-22 13:22:32 +0200

    Update discover_bike.py

--- pymoof/tools/discover_bike.py
+++ pymoof/tools/discover_bike.py
@@ -2,9 +2,10 @@ import asyncio
 
 from bleak import BleakScanner
 
+scanner = BleakScanner
 
-async def query():
-    devices = await BleakScanner.discover(
+async def query(scanner):
+    devices = await scanner.discover(
         service_uuids=[
             "6acc5540-e631-4069-944d-b8ca7598ad50",
             "8e7f1a50-087a-44c9-b292-a2c628fdd9aa",
