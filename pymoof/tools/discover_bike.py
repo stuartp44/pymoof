7dd9826c620f67bcfa71e20f1dece819210bfce3
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2024-06-23 22:25:47 +0200

    Update discover_bike.py

--- pymoof/tools/discover_bike.py
+++ pymoof/tools/discover_bike.py
@@ -8,15 +8,6 @@ async def query(scanner):
             "6acb5520-e631-4069-944d-b8ca7598ad50",
         ],
     )
-    for device in devices:
-        if "6acc5540-e631-4069-944d-b8ca7598ad50" in device.metadata["uuids"]:
-            print("Found SX3:", device.address)
-        if "8e7f1a50-087a-44c9-b292-a2c628fdd9aa" in device.metadata["uuids"]:
-            print("Found SX1/SX2:", device.address)
-        if "6acb5520-e631-4069-944d-b8ca7598ad50" in device.metadata["uuids"]:
-            print("Found Smart SX1:", device.address")
-    print("No Vanmoof bikes found")
-    
     return devices
 
 
